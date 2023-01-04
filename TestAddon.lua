c0d2fd02c4e595c26f45989327d292d6c4b3e962
Maeva De Campos <kulwilcoox@gmail.com>
2023-01-04 13:57:33 +0100

    Send message when entering instance
    Create a frame to subscribe event
    Check if the type of instance match desire instance, then send a message

--- TestAddon.lua
+++ TestAddon.lua
@@ -1,1 +1,11 @@
-print("Hello Azeroth!")
+local EventFrame = CreateFrame("frame", "EventFrame")
+EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+EventFrame:SetScript("OnEvent", function(self, event, ...)
+	if(event == "PLAYER_ENTERING_WORLD") then
+		local _, instanceType = ...
+        if(instanceType == nil) then
+            SendChatMessage("Hey all, bon jeu!", "PARTY")
+        end
+    end
+end)
