0433d947871a714d2b960100dac9edf9e393c019
Maéva De Campos <85650423+starkoreba@users.noreply.github.com>
2023-01-10 11:56:59 +0100

    Update TestAddon.lua

--- TestAddon.lua
+++ TestAddon.lua
@@ -4,7 +4,7 @@ EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 EventFrame:SetScript("OnEvent", function(self, event, ...)
 	if(event == "PLAYER_ENTERING_WORLD") then
 		local _, instanceType = ...
-        if(instanceType == nil) then
+        if(instanceType == "party") then
             SendChatMessage("Hey all, bon jeu!", "PARTY")
         end
     end
